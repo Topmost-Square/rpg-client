288701b65dcce3a1424bdad880874855fc637b08
keithchasen <keithchasen89@gmail.com>
2023-05-30 12:47:01 +0200

    resize canvas listener

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,43 @@
+import {useEffect, useRef, useState} from "react";
+
 export const Main = () => {
+    const canvasRef = useRef(null);
+
+    const [canvas, setCanvas] = useState(null)
+    const [context, setContext] = useState(null)
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            setCanvas(canvasRef.current);
+        }
+    },[canvasRef]);
+
+    useEffect(() => {
+        if (canvas) {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            setContext(canvas.getContext('2d'));
+        }
+    },[canvas]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (canvas) {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+            }
+
+        }
+        window.addEventListener('resize', handleResize)
+
+        return _=> window.removeEventListener('resize', handleResize);
+    })
+
     return (
         <div>
             <h1>MAIN</h1>
+            <canvas ref={canvasRef}></canvas>
         </div>
     );
 }
